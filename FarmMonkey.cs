2d3a1eb7fd64a610cd88ced2f4dfc7d1ca777aa6
Defectuous <defectuous@mac.com>
2015-03-09 04:40:20 -0600

    Update FarmMonkey.cs

--- FarmMonkey.cs
+++ FarmMonkey.cs
@@ -7,8 +7,8 @@ using System.Linq;
 using ArcheBuddy.Bot.Classes;
 
 //
-// Special Thanks for a really 
-//
+// Special Thanks to Voyager92 for a really Epic Non-Stop Farm/Gathering Base from which I am building this on. 
+// Thread:  [Plugin] Non-Stop Farm/Gathering for multiple toons/farms and with restart support
 //
 //
 //
@@ -23,23 +23,24 @@ namespace FarmMonkeyBeta
         { return "1.0.0.0"; }
         public static string GetPluginDescription()
         { return "FarmMonkey: Continuous Multi Farm Harvest & Planting Plugin"; }
-
-        // Start Universal Config
         
-        uint[] _farms = { 12345, 54321 };
+        // START Universal Config
+        
+        uint[] _farms = { 12345, 54321 }; // Gather Farm ID's wtih scarecrow
         int _minlabor   = 200;
         string _seed    = "Rice Seed";
         string _plant   = "Rice"; // Make sure plant is Mature type or not. 
+        // Note: You may need to update the Amount of labor needed for Gathering & Harvesting.
         string _gather  = "Gathering: Spend 1 Labor to gather materials.";
         string _harvest = "Farming: Spend 1 Labor to harvest crops.";
         //string _gpsfile = "\\Plugins\\FarmMonkey\\file.db3"; // Location of GPS File
 
         
-        // End Universal Config ( Do Not Edit anything past this line )
+        // END Universal Config 
+        // ( Do Not Edit anything past this line unless you are confident you know what your doing )
         
         // Universal Application Information
-        //int _labor = me.laborPoints;     // Not necessary I think.
-        //string _myname = me.name;   // Not necessary I think.
+
 
         //Call on plugin start
         public void PluginRun()
@@ -49,23 +50,39 @@ namespace FarmMonkeyBeta
             while (true) {    
                 if (gameState == GameState.Ingame){
                     Log("We are in game and ready to Farm");
-                    //MoveFromSafe();
                     // Time to Harvest plants
                     Harvesting();
                     // Lets fill that field
-                    PLanting();
+                    Planting();
+                    
+                    //  Temporary Sleep to prevent to many checks
+                    Random random = new Random();
+                    var mseconds  = random.Next(240, 300) * 1000;
+                    var seconds   = mseconds / 1000;
+                    Log("Waiting for " + seconds.ToString() + " seconds");
+                    Thread.Sleep(mseconds);
+
+                    //MoveFromSafe();
+                    //FarmCheckTime(); // Check time so we can come back and harvest when it's ready
                     //MovetoSafe();
+                    
                 }
             }
         }
 
-
+        // Move From Safe Location
         //public void MoveFromSafe()
         //{
 
         //}
 
+        // Move Back to Safe Location ( Possibley sit on nearest chair or bed )
         //public void MoveToSafe()
+        //{
+
+        //}
+        
+        //public void FarmCheckTime()
         //{
 
         //}
@@ -80,7 +97,7 @@ namespace FarmMonkeyBeta
                 } 
         }
 
-        public void PLanting()
+        public void Planting()
         {
             foreach (uint farm in _farms)
             {
@@ -88,6 +105,7 @@ namespace FarmMonkeyBeta
                 PlantItemsAtFarm(_seed, farm);
             }
         }
+        
         //Call on plugin stop
         public void PluginStop()
         {
