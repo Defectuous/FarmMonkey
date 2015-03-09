409fb567e9c5a5517e8c00c568ef108427506264
Defectuous <defectuous@mac.com>
2015-03-09 02:50:56 -0600

    Update FarmMonkey.cs

--- FarmMonkey.cs
+++ FarmMonkey.cs
@@ -13,31 +13,36 @@ using ArcheBuddy.Bot.Classes;
 //
 //
 
-namespace DefaultNameSpace{
-   public class DefaultClass : Core
-   {
-       public static string GetPluginAuthor()
-       { return "Defectuous"; }
-       public static string GetPluginVersion()
-       { return "1.0.0.0"; }
-       public static string GetPluginDescription()
-       { return "FarmMonkey: Continuous Multi Farm Harvest & Planting Plugin"; }
+namespace FarmMonkeyBeta
+{
+    public class FarmMonkey : Core
+    {
+        public static string GetPluginAuthor()
+        { return "Defectuous"; }
+        public static string GetPluginVersion()
+        { return "1.0.0.0"; }
+        public static string GetPluginDescription()
+        { return "FarmMonkey: Continuous Multi Farm Harvest & Planting Plugin"; }
 
-       // Start Universal Config
-       uint[] _farms = {12345, 54321};
-       int _minlabor   = 200;
-       string _seed    = "Rice Seed";
-       string _plant   = "Rice"; // Make sure seed is Mature type or not. 
-       string _gpsfile = "\\Plugins\\FarmMonkey\\file.db3"; // Location of GPS File
-       
-       
-       // End Universal Config ( Do Not Edit anything past this line )
-       // Universal Application Information
-       int _labor     = me.laborPoints;
-       // string _myname = me.name; 
-       
-       //Call on plugin start
-       public void PluginRun()
+        // Start Universal Config
+        
+        uint[] _farms = { 12345, 54321 };
+        int _minlabor   = 200;
+        string _seed    = "Rice Seed";
+        string _plant   = "Rice"; // Make sure plant is Mature type or not. 
+        string _gather  = "Gathering: Spend 1 Labor to gather materials.";
+        string _harvest = "Farming: Spend 1 Labor to harvest crops.";
+        //string _gpsfile = "\\Plugins\\FarmMonkey\\file.db3"; // Location of GPS File
+
+        
+        // End Universal Config ( Do Not Edit anything past this line )
+        
+        // Universal Application Information
+        //int _labor = me.laborPoints;     // Not necessary I think.
+        //string _myname = me.name;   // Not necessary I think.
+
+        //Call on plugin start
+        public void PluginRun()
         {
             ClearLogs();
             Log("FarmMonkey: Plugin Started");
@@ -50,48 +55,43 @@ namespace DefaultNameSpace{
                     // Lets fill that field
                     PLanting();
                     //MovetoSafe();
-                  else {
-                  Log("We are not in game yet") ;
-                 }
                 }
             }
         }
-        
-  
+
+
         //public void MoveFromSafe()
         //{
-        
+
         //}
-        
+
         //public void MoveToSafe()
         //{
-            
+
         //}
         
         public void Harvesting()
         {
             foreach (uint farm in _farms){
                 Log("Harvesting: "+farm);
-                if _labor >= _minlabor;
-                CollectItemsAtFarm(plant, "Gathering: Spend 1 Labor to gather materials.", farm);
-                CollectItemsAtFarm(plant, "Farming: Spend 1 Labor to harvest crops.", farm);
-                } else {
-                    Log("Your labor is low");
-                    Log("Labor:" + _labor);
-                }
+                if (me.laborPoints >= _minlabor)
+                CollectItemsAtFarm(_plant, _gather, farm);
+                CollectItemsAtFarm(_plant, _harvest, farm);
+                } 
         }
-        
+
         public void PLanting()
         {
-            foreach (uint farm in _farms){
-                Log("Planting: "+farm);
-                PlantItemsAtFarm(seed, farm);
-                }
+            foreach (uint farm in _farms)
+            {
+                Log("Planting: " + farm);
+                PlantItemsAtFarm(_seed, farm);
+            }
         }
-       //Call on plugin stop
-       public void PluginStop()
+        //Call on plugin stop
+        public void PluginStop()
         {
-            
+
         }
     }
 }
